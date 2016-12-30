8af49a28c3f6096d8412f492dfcb96c2c5f1013d
Phil B <phil.b.software@gmail.com>
2016-12-30 13:55:29 +0100

    Clean up code

---
+++ build/OH/CMakeFiles/OHPass.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OHPass.dir/OH.cpp.o"
+  "libOHPass.pdb"
+  "libOHPass.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OHPass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
